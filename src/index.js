651e9fd8ec5331fd5de2a38d00c827dfe002ea37
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-17 12:02:40 +0100

    Add a little bit of logic to <Footer/>

--- src/index.js
+++ src/index.js
@@ -26,6 +26,11 @@ function App() {
   }
 
   function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
     return (
       <footer>{new Date().toLocaleTimeString()} - We're currently open</footer>
     );
