1276b80830eb94aa544eab75dab8212699caad80
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-17 11:16:33 +0100

    moved data into src folder to import to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import pizzaData from './data';
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+
+  function Pizza() {
+    return <h2>Pizza</h2>;
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
